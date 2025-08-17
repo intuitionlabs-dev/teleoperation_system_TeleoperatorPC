0fda99b38ebe3030e79c01d559817f927a7384b0
BillyChern <shichen22@m.fudan.edu.cn>
2025-08-16 21:07:30 -0700

    update the ports

--- run_teleoperate.sh
+++ run_teleoperate.sh
@@ -9,8 +9,8 @@ python teleoperate.py \
     --robot-hostname 100.104.247.35 \
     --cmd-port 5555 \
     --fps 60 \
-    --left-leader-port /dev/tty.usbmodem5A680101071 \
-    --right-leader-port /dev/tty.usbmodem58FA0968181 \
+    --left-leader-port /dev/ttyACM1 \
+    --right-leader-port /dev/ttyACM0 \
     --calibration-dir calibration \
     --left-arm-calib-name left_arm \
     --right-arm-calib-name right_arm \
